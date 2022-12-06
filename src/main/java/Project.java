0ac4a270e9a0cb79abba3dfb1c476f788e6c5f2a
lennertsaerens <lennert.saerens@gmail.com>
2022-12-06 16:22:00 +0100

    Lennert queries

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -5,9 +5,9 @@ import net.ravendb.client.documents.session.IDocumentSession;
 import net.ravendb.client.documents.session.OrderingType;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Project {
     public static void main(String[] args) {
@@ -42,22 +42,44 @@ public class Project {
 //                System.out.println(E2);
 
                 // M4
-                List<String> M4 = session
-                        .query(Inproceedings.class)
-                        .whereRegex("Booktitle", "SIGMOD")
-                        .andAlso()
-                        .whereGreaterThan("Authors.Count", 10)
-                        .selectFields(String.class, "Title")
-                        .toList();
-                System.out.println(M4);
+//                List<Integer> M4Years = session
+//                        .query(Inproceedings.class)
+//                        .whereRegex("booktitle", "SIGMOD")
+//                        .andAlso()
+//                        .whereGreaterThan("authors.Count", 10)
+//                        .selectFields(Integer.class, "year")
+//                        .toList();
+//
+//                // Source: https://stackoverflow.com/questions/19031213/java-get-most-common-element-in-a-list
+//                Integer M4MostCommonYear = M4Years.stream()
+//                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+//                        .entrySet().stream().max((o1, o2) -> o1.getValue().compareTo(o2.getValue()))
+//                        .map(Map.Entry::getKey).orElse(null);
+//
+//                System.out.println(M4MostCommonYear);
 
                 // M5
-//                Integer M5 = session
+//                List<Proceedings> M5PODSProceedings = session
 //                        .query(Proceedings.class)
-//                        .whereEquals("Booktitle", "PODS")
-//                        .selectFields(String.class, "Title")
-//                        .count();
-//                System.out.println(M5);
+//                        .whereEquals("booktitle", "PODS")
+//                        .toList();
+//
+//                HashMap<String, Integer> EditorCount = new HashMap<String, Integer>();
+//                for(Proceedings proceeding: M5PODSProceedings) {
+//                    for(String editor: proceeding.getEditors()) {
+//                        Integer count = EditorCount.getOrDefault(editor, 0);
+//                        EditorCount.put(editor, count + 1);
+//                    }
+//                }
+//
+//                // Source: https://stackoverflow.com/questions/5911174/finding-key-associated-with-max-value-in-a-java-map
+//                String M5MostCommonEditor = Collections.max(EditorCount.entrySet(), Map.Entry.comparingByValue()).getKey();
+//                Integer M5MostCommonCount = EditorCount.get(M5MostCommonEditor);
+//
+//                System.out.println(M5MostCommonEditor + " : " + M5MostCommonCount);
+
+                // H2
+
             }
         }
     }
